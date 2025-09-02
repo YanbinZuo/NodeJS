b14f433463b106317bdea14564103fad8ca89abe
YanbinHCL <yanbin.zuo@hcltech.com>
2025-09-01 22:31:55 -0700

    EventEmitter practice

---
+++ 03Npm/index.js
@@ -0,0 +1,14 @@
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+
+// initialize object
+const myEmitter = new EventEmitter();
+
+// add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  // Emit event
+  myEmitter.emit("log", "Log event emitted!");
+});
