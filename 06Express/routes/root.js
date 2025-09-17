0a3bc7a39c39288fa7a0e954a79dbae78c29f632
YanbinHCL <yanbin.zuo@hcltech.com>
2025-09-17 14:29:40 -0700

    route practice

---
+++ 06Express/routes/root.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get(["/", "/index", "/index.html"], (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+router.get(["/new-page", "/new-page.html"], (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "new-page.html"));
+});
+
+router.get(/^\/old-page(\.html)?$/, (req, res) => {
+  res.redirect(301, "/new-page"); // 302 by default
+});
+
+module.exports = router;
--- 06Express/server.js
+++ 06Express/server.js
@@ -38,49 +38,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // built-in middleware to serve static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-app.get(["/", "/index", "/index.html"], (req, res) => {
-  // res.sendFile("./views/index.html", { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get(["/new-page", "/new-page.html"], (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get(/^\/old-page(\.html)?$/, (req, res) => {
-  res.redirect(301, "/new-page"); // 302 by default
-});
-
-// Route handlers
-app.get(
-  /^\/hello(\.html)?$/,
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World");
-  }
-);
-
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res) => {
-  console.log("three");
-  res.send("Finished!");
-};
-
-app.get(/^\/chain(\.html)?$/, [one, two, three]);
+// routes
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
 
 // need to use real regexp
 app.all(/.*/, (req, res) => {
